aaba38d05ed6cdd7e2da87fcc61c6673ad9080a5
remo7777 <28594846+remo7777@users.noreply.github.com>
2018-07-06 12:46:26 +0530

    Add files via upload

--- metasploit.sh
+++ metasploit.sh
@@ -1,51 +1,66 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-msfvar=4.16.12
-mkdir $HOME/tools
+cwd=$(pwd)
+#name=$(basename "$0")
+#export msfinst="$cwd/$name"
+#sha_actual=$(sha256sum $(echo $msfinst))
+#echo $sha_actual
+#if [ $name != "metasploit.sh" ]; then
+#	echo "[-] Please do not use third-party stolen scripts"
+#	exit 1
+#fi
 
+msfvar=4.16.50
+msfpath='/data/data/com.termux/files/home'
+if [ -d "$msfpath/metasploit-framework" ]; then
+	echo "i ll remove older one"
+	
+fi
 apt update
-apt install -y autoconf bison clang coreutils toilet curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev \
-    postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config \
-    postgresql-contrib wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
+apt install -y finch autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
 
-cd $HOME/tools
+cd $msfpath
 curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
-tar -xf $HOME/tools/$msfvar.tar.gz
-mv $HOME/tools/metasploit-framework-$msfvar $HOME/tools/metasploit-framework
-cd $HOME/tools/metasploit-framework
+tar -xf $msfpath/$msfvar.tar.gz
+mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
+cd $msfpath/metasploit-framework
 sed '/rbnacl/d' -i Gemfile.lock
 sed '/rbnacl/d' -i metasploit-framework.gemspec
 gem install bundler
+
+isNokogiri=$(gem list nokogiri -i)
+isGrpc=$(gem list grpc -i)
+
 sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
 
-gem install nokogiri -v'1.8.0' -- --use-system-libraries
- 
-sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/tools/metasploit-framework/Gemfile.lock
-gem unpack grpc -v 1.4.1
-cd grpc-1.4.1
-curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
-curl -L https://raw.githubusercontent.com/Auxilus/Auxilus.github.io/master/Grpc_extconf.patch -o extconf.patch
-patch -p1 < extconf.patch
-gem build grpc.gemspec
-gem install grpc-1.4.1.gem
-cd ..
-rm -r grpc-1.4.1
+if [ $isNokogiri == "false" ];
+then
+      gem install nokogiri -v'1.8.0' -- --use-system-libraries
+else
+	echo "nokogiri already installed"
+fi
 
-
-cd $HOME/tools/metasploit-framework
+cd $msfpath/metasploit-framework
 bundle install -j5
 
 echo "Gems installed"
 $PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
 rm ./modules/auxiliary/gather/http_pdf_authors.rb
-ln -s $HOME/tools/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
-ln -s $HOME/tools/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+
+if [ -e $PATH/bin/msfconsole ];then
+	rm $PATH/bin/msfconsole
+fi
+if [ -e $PATH/bin/msfvenom ];then
+	rm $PATH/bin/msfvenom
+fi
+ln -s $msfpath/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -s $msfpath/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
 
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
 echo "Creating database"
 
-cd $HOME/tools/metasploit-framework/config
-curl -LO https://Auxilus.github.io/database.yml
+cd $msfpath/metasploit-framework/config
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
 
 mkdir -p $PREFIX/var/lib/postgresql
 initdb $PREFIX/var/lib/postgresql
@@ -54,6 +69,6 @@ pg_ctl -D $PREFIX/var/lib/postgresql start
 createuser msf
 createdb msf_database
 
-
+rm $msfpath/$msfvar.tar.gz
 
 echo "you can directly use msfvenom or msfconsole rather than ./msfvenom or ./msfconsole as they are symlinked to $PREFIX/bin"
