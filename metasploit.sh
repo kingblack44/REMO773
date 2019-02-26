612bbee970dfbde51abe44b3353ffe9bfc268d15
remo7777 <28594846+remo7777@users.noreply.github.com>
2019-02-26 06:52:33 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -10,7 +10,7 @@ cwd=$(pwd)
 #	exit 1
 #fi
 
-msfvar=5.0.8
+msfvar=4.17.43
 msfpath='/data/data/com.termux/files/home'
 if [ -d "$msfpath/metasploit-framework" ]; then
 	echo "deleting old version..."
