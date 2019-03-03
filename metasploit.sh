8e35233387d9f40dfe5b052f02123b6d4c53d25a
remo7777 <28594846+remo7777@users.noreply.github.com>
2019-03-03 20:24:06 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -74,7 +74,7 @@ curl -LO https://Auxilus.github.io/database.yml
 mkdir -p $PREFIX/var/lib/postgresql
 initdb $PREFIX/var/lib/postgresql
 
-pg_ctl -D $PREFIX/var/lib/postgresqlart
+pg_ctl -D $PREFIX/var/lib/postgresql start
 createuser msf
 createdb msf_database
 
