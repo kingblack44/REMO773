a24c68ba776bf16d0479c334034d772a1fef9c81
remo7777 <28594846+remo7777@users.noreply.github.com>
2019-04-15 19:29:20 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -32,7 +32,7 @@ gem install pg --version=0.20.0 -- --use-system-libraries
 gem install nokogiri -v'1.8.5' -- --use-system-libraries
 cd $msfpath/metasploit-framework
 gem update --system
-gem install bundler --version "1.17."
+gem install bundler --version "1.17.3"
 bundle install -j5
 
 
