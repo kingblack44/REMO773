61321435670460de07cc1af1ec1a3e944aa03ff4
ramamajhi14 <ramamajhi14@gmail.com>
2019-04-25 23:23:11 +0530

    v5.0.19

--- metasploit.sh
+++ metasploit.sh
@@ -10,7 +10,7 @@ cwd=$(pwd)
 #	exit 1
 #fi
 
-msfvar=5.0.4
+msfvar=5.0.19
 msfpath='/data/data/com.termux/files/home'
 if [ -d "$msfpath/metasploit-framework" ]; then
 	echo "deleting old version..."
